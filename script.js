dbf1b9ef77be7e68c6ed38a790ca096f971fc43f
Eyad Mahmoud <123810537+eyadma7moud@users.noreply.github.com>
2026-02-26 21:20:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let isPlaying = true;
+
+// Swiching player function
+const swichPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;  
+    activePlayer = activePlayer === 0? 1 : 0; 
+
+    player0El.classList.toggle('player--active'); //toggle --> if class exist remove it and if not add it
+    player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    
+    if (isPlaying){
+        // 1. Generating a random dice roll 
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        console.log(dice);
+
+        // 2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+        
+        // 3. Check for rolled 1: if true, swich to next player
+        if (dice !==  1){
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+        } else {
+            // Switch to next player
+            swichPlayer();
+        }
+    }
+    
+
+});
+
+
+btnHold.addEventListener('click', function () {
+
+    if (isPlaying) {
+        console.log('Hold button');
+        // Add current score to active player
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent =
+        scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+            // Finish the game
+            document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.add('player--winner');
+            document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.remove('player--active');
+
+            isPlaying = false;
+            diceEl.classList.add('hidden');
+        } else {
+            // Swich player
+            swichPlayer();
+        }
+    }
+
+});
+ 
+// Reset the game
+btnNew.addEventListener('click', function () {
+    currentScore = 0;
+    scores[0] = 0;
+    scores[1] = 0;
+    score0El.textContent = scores[0];
+    score1El.textContent = scores[1];
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+    activePlayer = 0; 
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+
+    diceEl.classList.add('hidden');
+    isPlaying = true;
+});
